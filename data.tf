88378412f40d983d26ccd174d3b2607bad5559ed
mokainos <72137340+mokainos@users.noreply.github.com>
2021-05-28 15:30:37 +0100

    initial

--- data.tf
+++
@@ -1,8 +0,0 @@
-data "azurerm_client_config" "current" {}
-data "azurerm_key_vault" "kv" {
-  name                = var.azure_keyvault
-  resource_group_name = var.azure_keyvault_rg
-}
-data "azurerm_subscription" "subid" {
-  subscription_id = var.subscription_id
-}
