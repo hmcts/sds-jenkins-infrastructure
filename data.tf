bbb07afc5278e6e656e21036252fe71b12de1b05
mokainos <72137340+mokainos@users.noreply.github.com>
2021-06-01 08:43:20 +0100

    modified to accomodate ptl

--- data.tf
+++ data.tf
@@ -3,6 +3,3 @@ data "azurerm_key_vault" "kv" {
   name                = var.azure_keyvault
   resource_group_name = var.azure_keyvault_rg
 }
-data "azurerm_subscription" "subid" {
-  subscription_id = var.subscription_id
-}
