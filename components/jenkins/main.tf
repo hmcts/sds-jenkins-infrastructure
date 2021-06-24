dde4a857a53ae4bf0941c877d3ce140c0ebbd40f
Enda Kelly <enda.kelly@hmcts.net>
2021-06-24 09:29:41 +0100

    Updating resource group for disks

--- components/jenkins/main.tf
+++ components/jenkins/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_managed_disk" "disk" {
   name                 = "${var.product}-${var.env}-disk"
   location             = var.location
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.disks_resource_group.name
   storage_account_type = "Premium_LRS"
   create_option        = "Empty"
   disk_size_gb         = "1024"
