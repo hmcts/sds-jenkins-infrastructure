99a2bde29991007045961bcf001fce1f0eead950
mokainos <72137340+mokainos@users.noreply.github.com>
2021-06-30 10:00:11 +0100

    Update components/jenkins/keyvault.tf
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- components/jenkins/keyvault.tf
+++ components/jenkins/keyvault.tf
@@ -5,6 +5,7 @@ resource "azurerm_key_vault" "jenkinskv" {
   sku_name                   = "standard"
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days = 7
+  enable_rbac_authorization  = true
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
