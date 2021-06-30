cca3460fa1b8df645b32d4d224bd49ea708a96da
mokainos <72137340+mokainos@users.noreply.github.com>
2021-06-30 10:07:02 +0100

    updated kv name

--- components/jenkins/keyvault.tf
+++ components/jenkins/keyvault.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault" "jenkinskv" {
   location                   = var.location
-  name                       = "ptlsbox"
+  name                       = var.env
   resource_group_name        = azurerm_resource_group.rg.name
   sku_name                   = "standard"
   tenant_id                  = data.azurerm_client_config.current.tenant_id
