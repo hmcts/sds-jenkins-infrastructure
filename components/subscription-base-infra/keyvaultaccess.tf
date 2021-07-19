474381880d2894512cbcc9c51fa8551f4cfdab28
mokainos <72137340+mokainos@users.noreply.github.com>
2021-07-19 13:35:31 +0100

    DTSPO-2879 updated object id (#19)
    * keyvault permissions
    
    * updated objecid to point to jenkins mi
    
    * typo

--- components/subscription-base-infra/keyvaultaccess.tf
+++ components/subscription-base-infra/keyvaultaccess.tf
@@ -8,7 +8,7 @@ data "azurerm_key_vault" "subscription" {
 resource "azurerm_key_vault_access_policy" "subscriptionaccess" {
   key_vault_id = data.azurerm_key_vault.subscription.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+  object_id    = var.env == "ptlsbox" ? var.jenkins-ptlsbox-mi : var.jenkins-ptl-mi
 
   secret_permissions = [
     "Get",
--- components/subscription-base-infra/variables.tf
+++ components/subscription-base-infra/variables.tf
@@ -17,3 +17,15 @@ variable "location" {
   type        = string
   default     = "UK South"
 }
+
+variable "jenkins-ptlsbox-mi" {
+  description = "Objectid for jenkins managed identity"
+  type        = string
+  default     = "6df94cb5-c203-4493-bc8a-3f6aad1133e1"
+}
+
+variable "jenkins-ptl-mi" {
+  description = "Objectid for  jenkins managed identity"
+  type        = string
+  default     = "7ef3b6ce-3974-41ab-8512-c3ef4bb8ae01"
+}
