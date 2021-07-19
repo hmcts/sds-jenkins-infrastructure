1aa674575b9b570a2404599a739bd26a720883fd
mokainos <72137340+mokainos@users.noreply.github.com>
2021-07-19 12:06:38 +0100

    keyvault permissions (#18)

---
+++ components/subscription-base-infra/keyvaultaccess.tf
@@ -0,0 +1,17 @@
+data "azurerm_client_config" "current" {}
+
+data "azurerm_key_vault" "subscription" {
+  name                = "dtssharedservices${var.env}kv"
+  resource_group_name = "genesis-rg"
+}
+
+resource "azurerm_key_vault_access_policy" "subscriptionaccess" {
+  key_vault_id = data.azurerm_key_vault.subscription.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+  ]
+}
