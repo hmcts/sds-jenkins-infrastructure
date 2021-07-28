6ec0145bbaa168be704a3f57e2caa06094f76319
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2021-07-28 13:45:58 +0100

    DTSPO-2880 - adding required kv secrets for prod (#25)
    * DTSPO-2880 - adding required kv secrets for prod
    
    * Adding in condition to create only in ptl env
    
    * moved secrets resource into base-infra component
    
    * formatting data.tf
    
    * Adding tfvars for each sub
    
    * Undoing previous changes and using foreach for env_subscription_id
    
    * Removing env_subscription_id from demo.tfvars
    
    * Update components/subscription-base-infra/main.tf
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    
    * Formatting in keyvault.tf
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- components/jenkins/locals.tf
+++ components/jenkins/locals.tf
@@ -1,15 +1,14 @@
 locals {
-
   ptl = {
 
-    DTS-SHAREDSERVICES-STG  = "74dacd4f-a248-45bb-a2f0-af700dc4cf68",
-    DTS-SHAREDSERVICES-TEST = "3eec5bde-7feb-4566-bfb6-805df6e10b90",
-    DTS-SHAREDSERVICES-DEMO = "c68a4bed-4c3d-4956-af51-4ae164c1957c",
-    DTS-SHAREDSERVICES-ITHC = "ba71a911-e0d6-4776-a1a6-079af1df7139",
-    DTS-SHAREDSERVICES-PROD = "5ca62022-6aa2-4cee-aaa7-e7536c8d566c",
-    DTS-SHAREDSERVICES-DEV  = "867a878b-cb68-4de5-9741-361ac9e178b6"
+    stg  = "74dacd4f-a248-45bb-a2f0-af700dc4cf68",
+    test = "3eec5bde-7feb-4566-bfb6-805df6e10b90",
+    demo = "c68a4bed-4c3d-4956-af51-4ae164c1957c",
+    ithc = "ba71a911-e0d6-4776-a1a6-079af1df7139",
+    prod = "5ca62022-6aa2-4cee-aaa7-e7536c8d566c",
+    dev  = "867a878b-cb68-4de5-9741-361ac9e178b6"
   }
   ptlsbox = {
-    DTS-SHAREDSERVICES-SBOX = "a8140a9e-f1b0-481f-a4de-09e2ee23f7ab"
+    sbox = "a8140a9e-f1b0-481f-a4de-09e2ee23f7ab"
   }
 }
