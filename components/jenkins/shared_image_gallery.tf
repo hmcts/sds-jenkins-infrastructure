eb699f561b5da2da4a4ccbd4d9b67fd315168be8
msl8r <72858053+msl8r@users.noreply.github.com>
2021-11-24 15:12:43 +0000

    Reduce permissions on ImageGallery for jenkins mi's (#28)

--- components/jenkins/shared_image_gallery.tf
+++ components/jenkins/shared_image_gallery.tf
@@ -6,6 +6,6 @@ data "azurerm_shared_image_gallery" "imagegallery" {
 resource "azurerm_role_assignment" "imagegallery" {
   provider             = azurerm.image_gallery
   scope                = data.azurerm_shared_image_gallery.imagegallery.id
-  role_definition_name = "Contributor"
+  role_definition_name = "Reader"
   principal_id         = azurerm_user_assigned_identity.usermi.principal_id
 }
