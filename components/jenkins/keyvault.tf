d9f688a5250653cd177f03166a6863b3fdc5ed85
Zoë <54357991+zc-hmcts@users.noreply.github.com>
2022-07-21 10:09:32 +0100

    Update keyvault.tf

--- components/jenkins/keyvault.tf
+++ components/jenkins/keyvault.tf
@@ -10,7 +10,7 @@ resource "azurerm_key_vault" "jenkinskv" {
 }
 
 resource "azurerm_key_vault" "jenkinskv-prod" {
-  count                      = var.env == "prod" ? 0 : 1
+  count                      = var.env == "ptl" ? 0 : 1
   location                   = var.location
   name                       = "prod-vault"
   resource_group_name        = azurerm_resource_group.rg.name
