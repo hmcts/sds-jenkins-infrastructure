f7b077b0b2d550c68b2149bd187c5b29603a39e2
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-02-03 13:43:03 +0000

    update

--- components/subscription-base-infra/main.tf
+++ components/subscription-base-infra/main.tf
@@ -1,8 +1,9 @@
 module "tags" {
-  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment = lower(var.env)
-  product     = var.product
-  builtFrom   = var.builtFrom
+  source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment  = lower(var.env)
+  product      = var.product
+  builtFrom    = var.builtFrom
+  expiresAfter = "3000-01-01"
 }
 
 resource "azurerm_resource_group" "rg" {
