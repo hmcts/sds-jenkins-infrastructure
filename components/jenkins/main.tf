3f2bf0c279c6579e3b2a62a87c42dae32ad9c64d
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-02-03 10:29:05 +0000

    fmt

--- components/jenkins/main.tf
+++ components/jenkins/main.tf
@@ -7,10 +7,10 @@
 
 
 module "tags" {
-  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment = lower(var.env)
-  product     = var.product
-  builtFrom   = var.builtFrom
+  source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment  = lower(var.env)
+  product      = var.product
+  builtFrom    = var.builtFrom
   expiresAfter = "3000-01-01"
 }
 
