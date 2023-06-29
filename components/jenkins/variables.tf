4b2810d0ba9a7db0bf52048de3727dd9d24a4adc
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-06-29 14:47:40 +0100

    update_variables

--- components/jenkins/variables.tf
+++ components/jenkins/variables.tf
@@ -36,3 +36,29 @@ variable "partition_key" {
 }
 
 variable "private_dns_subscription_id" {}
+
+variable "servicebus_enable_private_endpoint" {
+  description = "Enable private endpoint."
+  default     = true
+}
+variable "queue_name" {
+  default     = "jenkins"
+  description = "Name of the servicebus Queue."
+}
+variable "zone_redundant" {
+  description = "Enable Zone redundancy."
+  default     = false
+}
+variable "enable_workflow" {
+  description = "Enable workflow"
+  default     = true
+}
+variable "expiresAfter" {
+  description = "Expiration date"
+  default     = "3000-01-01"
+}
+
+variable "subscription_id" {
+  description = "Subscription to run against"
+  type        = string
+  }
