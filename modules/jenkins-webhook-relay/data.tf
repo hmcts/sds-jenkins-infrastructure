67f4d29602f4c1327a1ce3090bc15a21bc0c88f5
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-06-29 16:03:13 +0100

    update

--- modules/jenkins-webhook-relay/data.tf
+++ modules/jenkins-webhook-relay/data.tf
@@ -5,12 +5,6 @@ module "ctags" {
   builtFrom    = var.builtFrom
   expiresAfter = var.expiresAfter
 }
-
-data "azurerm_key_vault" "kv" {
-  name                = var.key_vault_name
-  resource_group_name = var.key_vault_rg_name
-}
-
 data "azurerm_managed_api" "api" {
   name     = "servicebus"
   location = var.location
