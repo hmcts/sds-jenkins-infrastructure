75e4d67851a8946c96b362383ffe65816871582a
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-09-20 15:04:34 +0100

    Update integration.tf

--- components/jenkins/integration.tf
+++ components/jenkins/integration.tf
@@ -15,7 +15,7 @@ module "jenkins-webhook-relay" {
   zone_redundant                     = var.zone_redundant
   enable_workflow                    = var.enable_workflow
   common_tags                        = module.tags.common_tags
-  max_delivery_count                 = 200
+  max_delivery_count                 = 50
 }
 
 resource "azurerm_key_vault_secret" "logicappsecret" {
