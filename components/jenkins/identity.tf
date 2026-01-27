07a130adc46f15b146ade217dcf6a6ada4d1525d
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-01-27 15:25:03 +0000

    DTSPO-29750 - Add new temporary uami for ptlsbox (#99)

---
+++ components/jenkins/identity.tf
@@ -0,0 +1,39 @@
+resource "azurerm_user_assigned_identity" "usermi2" {
+  count               = var.env == "ptlsbox" ? 1 : 0
+  resource_group_name = data.azurerm_resource_group.mi.name
+  location            = var.location
+  name                = "dtspo-29750"
+  tags                = local.common_tags
+}
+
+resource "azurerm_role_assignment" "vm_contributor" {
+  count = var.env == "ptlsbox" ? 1 : 0
+
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  role_definition_name = "VM Contributor"
+  principal_id         = azurerm_user_assigned_identity.usermi2[0].principal_id
+}
+
+resource "azurerm_role_assignment" "network_contributor" {
+  count = var.env == "ptlsbox" ? 1 : 0
+
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_user_assigned_identity.usermi2[0].principal_id
+}
+
+resource "azurerm_role_assignment" "compute_gallery_image_reader" {
+  count = var.env == "ptlsbox" ? 1 : 0
+
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  role_definition_name = "Compute Gallery Image Reader"
+  principal_id         = azurerm_user_assigned_identity.usermi2[0].principal_id
+}
+
+resource "azurerm_role_assignment" "hmctsacrpull2" {
+  count = var.env == "ptlsbox" ? 1 : 0
+
+  scope                = data.azurerm_resource_group.acr_rg.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.usermi2[0].principal_id
+}
