559b6d8f650ce262bc5ec58330697d475d98bf2b
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-01-27 15:45:29 +0000

    fix typo in role name (#100)

--- components/jenkins/identity.tf
+++ components/jenkins/identity.tf
@@ -10,7 +10,7 @@ resource "azurerm_role_assignment" "vm_contributor" {
   count = var.env == "ptlsbox" ? 1 : 0
 
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  role_definition_name = "VM Contributor"
+  role_definition_name = "Virtual Machine Contributor"
   principal_id         = azurerm_user_assigned_identity.usermi2[0].principal_id
 }
 
