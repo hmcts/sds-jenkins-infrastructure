fafbee038f602f8616061b977bd7328408b29d7d
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-01-28 09:16:05 +0000

    DTSPO-29750 - Move identity to ptl (#101)

--- components/jenkins/identity.tf
+++ components/jenkins/identity.tf
@@ -1,5 +1,5 @@
 resource "azurerm_user_assigned_identity" "usermi2" {
-  count               = var.env == "ptlsbox" ? 1 : 0
+  count               = var.env == "ptl" ? 1 : 0
   resource_group_name = data.azurerm_resource_group.mi.name
   location            = var.location
   name                = "dtspo-29750"
@@ -7,7 +7,7 @@ resource "azurerm_user_assigned_identity" "usermi2" {
 }
 
 resource "azurerm_role_assignment" "vm_contributor" {
-  count = var.env == "ptlsbox" ? 1 : 0
+  count = var.env == "ptl" ? 1 : 0
 
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
   role_definition_name = "Virtual Machine Contributor"
@@ -15,7 +15,7 @@ resource "azurerm_role_assignment" "vm_contributor" {
 }
 
 resource "azurerm_role_assignment" "network_contributor" {
-  count = var.env == "ptlsbox" ? 1 : 0
+  count = var.env == "ptl" ? 1 : 0
 
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
   role_definition_name = "Network Contributor"
@@ -23,15 +23,23 @@ resource "azurerm_role_assignment" "network_contributor" {
 }
 
 resource "azurerm_role_assignment" "compute_gallery_image_reader" {
-  count = var.env == "ptlsbox" ? 1 : 0
+  count = var.env == "ptl" ? 1 : 0
 
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
   role_definition_name = "Compute Gallery Image Reader"
   principal_id         = azurerm_user_assigned_identity.usermi2[0].principal_id
 }
 
+resource "azurerm_role_assignment" "ptlcontributor" {
+
+  for_each             = var.env == "ptl" ? (data.azurerm_client_config.current.subscription_id == "6c4d2513-a873-41b4-afdd-b05a33206631" ? local.ptl : local.ptlsbox) : {}
+  scope                = "/subscriptions/${each.value}"
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_user_assigned_identity.usermi2[0].principal_id
+}
+
 resource "azurerm_role_assignment" "hmctsacrpull2" {
-  count = var.env == "ptlsbox" ? 1 : 0
+  count = var.env == "ptl" ? 1 : 0
 
   scope                = data.azurerm_resource_group.acr_rg.id
   role_definition_name = "AcrPull"
