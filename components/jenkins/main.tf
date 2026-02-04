ade5b81a8359ccebba6fb88245d45b3304bf339a
shabmama53 <shabbir.mama@justice.gov.uk>
2026-02-04 12:21:05 +0000

    adding zone to the disk (#104)
    * adding zone to the disk
    
    * adding zone to the disk

--- components/jenkins/main.tf
+++ components/jenkins/main.tf
@@ -25,11 +25,10 @@ resource "azurerm_managed_disk" "disk" {
   name                 = "${var.product}-${var.env}-disk"
   location             = var.location
   resource_group_name  = data.azurerm_resource_group.disks_resource_group.name
-  storage_account_type = "Premium_LRS"
+  storage_account_type = "Premium_ZRS"
   create_option        = "Empty"
   disk_size_gb         = "1024"
   tags                 = local.common_tags
-  zone                 = "1"
 
 }
 
