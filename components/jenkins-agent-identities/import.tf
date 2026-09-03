2ef1aa9362392cc13b8f4e5e6497ce2716ad99ad
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-03 14:50:50 +0100

    import sbox role

---
+++ components/jenkins-agent-identities/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = azurerm_role_assignment.storage_account_data_contributor[0]
+  id = "/subscriptions/a8140a9e-f1b0-481f-a4de-09e2ee23f7ab/providers/Microsoft.Authorization/roleAssignments/707d9007-37b3-5349-b34f-11f6cc87982a"
+}
