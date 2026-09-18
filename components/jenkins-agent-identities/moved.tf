d9edebc55ca03adf9d7a82fda6e7c2104a0bf6d7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-18 09:48:05 +0100

    remove moved block

--- components/jenkins-agent-identities/moved.tf
+++ components/jenkins-agent-identities/moved.tf
@@ -1,9 +1,4 @@
 moved {
-  from = azurerm_role_assignment.storage_account_data_contributor[0]
-  to   = azurerm_role_assignment.rbac_administrator["Storage Account Data Contributor"]
-}
-
-moved {
   from = azurerm_role_assignment.storage_account_contributor[0]
   to   = azurerm_role_assignment.rbac_administrator["Storage Account Contributor"]
 }
